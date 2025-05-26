526fac57f4c47364b22b5f68ec664f4f3f7cc998
surendra <surendraredddyalamuru01@gmail.com>
2025-05-26 10:22:48 +0530

    roboshop-terraform

--- main.tf
+++ main.tf
@@ -8,10 +8,10 @@ module "db-instances" {
   env = var.env
   zone_id = var.zone_id
   component_name = each.key
-  instance_type = each.value.instance_type
-  app_port = each.value.app_port
+  instance_type = each.value["instance_type"]
+  app_port = each.value["app_port"]
   domain_name = var.domain_name
-  volume_size = each.value.volume_size
+  volume_size = each.value["volume_size"]
   vault_token    = var.vault_token
 }
 
@@ -21,10 +21,10 @@ module "app-instances" {
   zone_id = var.zone_id
   env = var.env
   component_name = each.key
-  instance_type = each.value.instance_type
-  app_port = each.value.app_port
+  instance_type = each.value["instance_type"]
+  app_port = each.value["app_port"]
   domain_name = var.domain_name
-  volume_size = each.value.volume_size
+  volume_size = each.value["volume_size"]
   vault_token    = var.vault_token
 }
 
@@ -34,13 +34,9 @@ module "web-instances" {
   env = var.env
   zone_id = var.zone_id
   component_name = each.key
-  instance_type = each.value.instance_type
-  app_port = each.value.app_port
+  instance_type = each.value["instance_type"]
+  app_port = each.value["app_port"]
   domain_name = var.domain_name
-  volume_size = each.value.volume_size
+  volume_size = each.value["volume_size"]
   vault_token    = var.vault_token
 }
-
-#variable "vault_token" {
-#  default = "hvs.nZys8APfnymhUuR0vgbAmR8A"
-#}
