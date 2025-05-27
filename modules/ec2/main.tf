73f3144562084082493a1b4d6ba9547bc29648ba
surendra <surendraredddyalamuru01@gmail.com>
2025-05-27 11:57:09 +0530

    roboshop-terraform

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -71,8 +71,7 @@ resource "null_resource" "ansible-pull" {
     }
 
     inline = [
-      "sudo dnf install ansible-core.x86_64",
-      "sudo ansible-pull -i localhost, -U https://github.com/surendraalamuru22/roboshop-ansible roboshop.yml -e env=${var.env} -e COMPONENT=${var.component_name} -e vault_token=${var.vault_token}"
+      "ansible-pull -i localhost, -U https://github.com/surendraalamuru22/roboshop-ansible roboshop.yml -e env=${var.env} -e COMPONENT=${var.component_name} -e vault_token=${var.vault_token}"
     ]
   }
 }
